badc048af506efe1d14b6a6c88d530975577862b
Fabrizio Grosa <fabrizio@mbp-di-fabrizio-001.homenet.telecomitalia.it>
2016-11-01 12:33:05 +0100

    wrong directory deleted

--- KFParticle/kfparticle/KFParticleTest/KFParticleTest.C
+++
@@ -1,6 +0,0 @@
-void KFParticleTest() {
-  if (! TClass::GetClass("KFParticle")) gSystem->Load("KFParticle");
-  if (! TClass::GetClass("KFParticleTest")) gSystem->Load("KFParticleTest");
-  KFParticleTest kfptest; 
-  kfptest.RunTest();
-}
